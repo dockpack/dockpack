44d9a3bada6326039ec63ad428050fa03ebf5a7b
Bas Meijer <bas.meijer@me.com>
2015-06-05 16:27:55 +0200

    not installing SourceTree

--- scripts/chocopacks.bat
+++ scripts/chocopacks.bat
@@ -3,6 +3,7 @@ set /p PATH=<C:\Windows\Temp\PATH
 
 :: Install all the things; for example:
 cmd /c choco install -y notepadplusplus Firefox GoogleChrome
-cmd /c choco install -y jre8 eclipse git SourceTree
+cmd /c choco install -y jre8 eclipse git
 cmd /c choco install -y ant maven gradle
+:: SourceTree triggers the install of .Net Framework 4.5
 
