c3fac7526ef5aac861777b5877b43baabbb213d8
Bas Meijer <bas.meijer@me.com>
2015-11-07 09:26:13 +0100

    Cleanup of scripts

--- scripts/chocopacks.bat
+++
@@ -1,9 +0,0 @@
-:: Ensure C:\Chocolatey\bin is on the path
-set /p PATH=<C:\Windows\Temp\PATH
-
-:: Install all the things; for example:
-cmd /c choco install -y notepadplusplus Firefox GoogleChrome
-cmd /c choco install -y jre8 eclipse git
-cmd /c choco install -y ant maven gradle
-:: SourceTree triggers the install of .Net Framework 4.5
-
