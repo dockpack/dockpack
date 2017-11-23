3c1b05536dc074c1b8a68f32922e39a0f148caa9
Bas Meijer <bas.meijer@me.com>
2017-11-24 00:29:29 +0100

    Creating an signing key for ssh certificates Create an ansible user with ssh key pair Install the ca_key.pub with a file provisioner in packer Enable signed ssh certificates in /etc/ssh/sshd_config Sign the public key of the ansible user

--- scripts/ansible-centos.sh
+++ scripts/ansible-centos.sh
@@ -1,7 +1,25 @@
 #!/bin/bash -eux
+# prepare centos virtual machine for use of ansible
 
-# Install EPEL repository.
-rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+yum update -y ca-certificates
+yum install --assumeyes epel-release
+yum install --assumeyes libcurl-devel git python-devel python-pyasn1 python-crypto python-cryptography python-pip
 
-# Install Ansible.
-yum -y install ansible python-setuptools
+pip install setuptools
+pip install --upgrade --force pip
+pip install ansible
+
+# create ansible user and group
+/usr/sbin/groupadd ansible
+/usr/sbin/useradd ansible -g ansible -G wheel -d /home/ansible -c "dockpack"
+
+# add ssh signer's public key - user can ssh with signed certificate
+echo 'TrustedUserCAKeys /etc/ssh/ca_key.pub' >> /etc/ssh/sshd_config
+
+# give sudo access (grants all permissions to user ansible)
+echo "ansible ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ansible
+chmod 0440 /etc/sudoers.d/ansible
+
+# set password
+echo "ansible" | passwd --stdin ansible
